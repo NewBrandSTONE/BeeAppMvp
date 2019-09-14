dcc5e62ce6ca70b8aa12f1da61736e2bff334fbd
NewBrandSTONE <187336604@qq.com>
2019-09-14 12:06:05 +0800

    项目初始化

---
+++ library_common/src/main/java/com/young/android/library/common/models/base/BaseRequest.java
@@ -0,0 +1,57 @@
+package com.young.android.library.common.models.base;
+
+/**
+ * 请求包装类
+ *
+ * @author O.z Young
+ * @version 2019-09-13
+ */
+public class BaseRequest<T> extends BaseModel {
+
+    private String reqId;
+    private String sign;
+    private String token;
+    private T data;
+
+    public String getReqId() {
+        return reqId;
+    }
+
+    public void setReqId(String reqId) {
+        this.reqId = reqId;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseRequest{" +
+                "reqId='" + reqId + '\'' +
+                ", sign='" + sign + '\'' +
+                ", token='" + token + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
---
+++ library_common/src/main/java/com/young/android/library/common/models/init/DeviceInitRequest.java
@@ -0,0 +1,30 @@
+package com.young.android.library.common.models.init;
+
+import com.young.android.library.common.models.base.BaseModel;
+
+/**
+ * 设备信息请求对象
+ *
+ * @author O.z Young
+ * @version 2019-09-13
+ */
+public class DeviceInitRequest extends BaseModel {
+
+    private String deviceImei;
+
+    public String getDeviceImei() {
+        return deviceImei;
+    }
+
+    public void setDeviceImei(String deviceImei) {
+        this.deviceImei = deviceImei;
+    }
+
+    @Override
+    public String toString() {
+        return "DeviceInitRequest{" +
+                "deviceImei='" + deviceImei + '\'' +
+                '}';
+    }
+
+}
