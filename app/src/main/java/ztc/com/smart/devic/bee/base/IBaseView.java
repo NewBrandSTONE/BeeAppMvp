cb7779cd5d1218b13c34da430927143071d6fb2b
NewBrandSTONE <187336604@qq.com>
2019-09-22 16:58:59 +0800

    增加多个Module包并且使用config.gradle统一管理依赖包版本

--- app/src/main/java/ztc/com/smart/devic/bee/base/IBaseView.java
+++ app/src/main/java/ztc/com/smart/devic/bee/base/IBaseView.java
@@ -3,11 +3,19 @@ package ztc.com.smart.devic.bee.base;
 /**
  * 给BaseActivity使用的规定的方法
  * 用来控制加载框的展示以及隐藏
+ *
+ * @author jonesleborn
  */
 public interface IBaseView {
 
+    /**
+     * 弹出加载框
+     */
     void showLoading();
 
+    /**
+     * 隐藏加载框
+     */
     void hideLoading();
 
 }
